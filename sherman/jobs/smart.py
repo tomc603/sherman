36e545bf1dd24bfeff1093a3096693afb08b8b9a
Tom Cameron <tomc603@gmail.com>
2022-02-25 17:21:45 -0500

    Refactor for modules
    Move the connection handling into a class.
    Clean up children and idle parents on connection close.
    Move connection logic into sherman script.
    Remove connection logic from child module.
    Begin the migration to fabric.

--- sherman/jobs/smart.py
+++ sherman/jobs/smart.py
@@ -1,116 +1,14 @@
 #!/usr/bin/env python3
 
-import argparse
+import logging
+import re
 
 import fabric
-import getpass
-import logging
-import multiprocessing as mp
-import os
-import paramiko
-import re
-import socket
-import sys
-
 
 logging.basicConfig(level=logging.DEBUG)
 logging.getLogger(__name__).setLevel(logging.DEBUG)
-logging.getLogger('paramiko').setLevel(logging.DEBUG)
-
-
-DEFAULT_TIMEOUT = 10.0
-DEFAULT_PORT = 22
-PROMPT_SUDO_RE = r'\[sudo\] password for '
-PROMPT_2FA_RE = r'Duo two-factor login'
-
-# Cache for host connections
-connection_registry = {
-    'gateway.host.tld': {
-        'connection': fabric.Connection,
-        'parent': None,
-        'children': ['proxy.host.tld', 'some_other.host.tld']
-    },
-    'proxy.host.tld': {
-        'connection': fabric.Connection,
-        'parent': 'gateway.host.tld',
-        'children': ['example.host.tld']
-    },
-    'example.host.tld': {
-        'connection': fabric.Connection,
-        'parent': 'proxy.host.tld'
-    },
-    'some_other.host.tld': {
-        'connection': fabric.Connection,
-        'parent': 'gateway.host.tld'
-    }
-}
-
-logging.basicConfig(level=logging.DEBUG)
 logger = logging.getLogger()
 logger.setLevel(logging.WARNING)
-
-
-def close_connection(hostname: str):
-    """
-    Close the connection for the specified host. If the connection has child connections, iterate through them,
-    calling this function recursively. Remove our child entry if we have a parent connection (gateway/proxy).
-    Finally, remove our connection entry from connection_registry.
-
-    :param hostname: Host name of the connected host.
-    :type hostname: str
-    """
-    try:
-        children = connection_registry[hostname]['children']
-        if children:
-            for child in children:
-                close_connection(child)
-    except KeyError:
-        pass
-
-    try:
-        parent = connection_registry[hostname]['parent']
-        if parent:
-            connection_registry[parent]['children'].remove(hostname)
-    except KeyError:
-        pass
-
-    try:
-        connection = connection_registry[hostname]['connection']
-        connection.close()
-        del(connection_registry[hostname])
-    except KeyError:
-        pass
-
-
-def open_connection(hostname: str):
-    """
-    Open a connection for the specified host. If the host has a ProxyJump server specified, connect through that proxy
-     and add a a child entry to its children list. Finally, add our connection entry to connection_registry.
-
-    :param hostname: Host name of the desired host.
-    :type hostname: str
-    :return A Connection for the specified hostname
-    :rtype fabric.Connection
-    """
-    if hostname in connection_registry:
-        return connection_registry[hostname]['connection']
-
-    ssh_config = fabric.config.SSHConfig().from_path('.sshconfig')
-    fab_config = fabric.Config(ssh_config=ssh_config)
-    fab_config.load_base_conf_files()
-
-    # If a ProxyJump gateway is configured, pass it as the gateway for the current connection.
-    # Cache the gateway connection so we can reuse it for future host connections
-    gateway = None
-    gateway_hostname = ''
-    if 'proxyjump' in ssh_config.lookup(hostname):
-        gateway_hostname = ssh_config.lookup(hostname)['proxyjump']
-        gateway = open_connection(gateway_hostname)
-        connection_registry[gateway_hostname]['children'].append(hostname)
-
-    connection = fabric.Connection(hostname, gateway=gateway)
-    connection_registry[hostname] = {'children': [], 'parent': gateway_hostname, 'connection': connection}
-    return connection_registry[hostname]['connection']
 
 
 def fab_smartctl(c: fabric.Connection, logpath: str = None):
@@ -130,77 +28,9 @@ def fab_smartctl(c: fabric.Connection, logpath: str = None):
         r = c.sudo(f'/usr/sbin/smartctl -x /dev/{scsi_device}')
 
 
-def client(hostname, username, logpath):
-    c = open_connection(hostname)
-    if c is None:
-        print(f'Connection to {hostname} failed.')
-        return
-
-    try:
-        fab_smartctl(c, logpath=logpath)
-    except paramiko.AuthenticationException as ex:
-        print(f'Error: Authentication error for {username}@{hostname}. {ex}')
-        return
-    except paramiko.BadHostKeyException as ex:
-        print(f'Error: Bad host key for {hostname}. {ex}')
-        return
-    except paramiko.SSHException as ex:
-        print(f'Error: SSH exception for {hostname}. {ex}')
-        return
-    except socket.error as ex:
-        print(f'Error: Socket error for {hostname}. {ex}')
-        return
-    finally:
-        # Close the SSH connection
-        close_connection(hostname)
+def run(c: fabric.Connection, logpath):
+    fab_smartctl(c, logpath=logpath)
 
 
 if __name__ == '__main__':
-    host_list = []
-    # TODO:
-    #   Use Fabric Group management to handle a pool of devices
-    #     * fabric.group.ThreadingGroup
-    #   Retrieve results from commands and write to local per-server log files
-
-    # TODO: When user is NONE, use the SSH config, then fall back to current OS user.
-    # TODO: If user _is_ specified, use it before SSH config.
-    ap = argparse.ArgumentParser(description='Collect SMART data from remote hosts.')
-    ap.add_argument('-d', '--debug', action='store_true', help='Enable diagnostic output.')
-    ap.add_argument('-H', '--hostfile', nargs='?', type=str, help='File containing a list of hosts.')
-    ap.add_argument('hosts', nargs='*', type=str, help='One or more hosts to poll.')
-    ap.add_argument('-l', '--logpath', nargs='?', type=str, help='Path to output host log files.')
-    ap.add_argument('-p', '--parallel', type=int, default=10, help='Path to output host log files.')
-    ap.add_argument('-u', '--user', type=str, default=getpass.getuser(), help='Remote host username.')
-    args = ap.parse_args()
-
-    if args.logpath is not None and args.logpath != '':
-        if not os.path.isdir(args.logpath):
-            print(f'Error: Log path {args.logpath} does not exist.')
-            sys.exit(1)
-
-    if args.hostfile is not None:
-        # A file of hosts was specified on the command line
-        with open(args.hostfile, 'r') as host_file:
-            host_lines = host_file.readlines()
-            for host_line in host_lines:
-                host_list.append(host_line.strip())
-    elif args.hosts:
-        # A list of hosts was specified on the command line
-        host_list = args.hosts.copy()
-    else:
-        # The command line list is empty, and there was no host file specified
-        print('Error: You must specify a host, or a file containing a list of hosts.')
-        ap.print_help()
-        sys.exit(1)
-
-    if args.parallel < 1:
-        print(f'Error: Parallel must be greater than 0.')
-        ap.print_help()
-        sys.exit(1)
-
-    worker_pool = mp.Pool(processes=args.parallel)
-    [worker_pool.apply(client, args=(host, args.user, args.logpath,)) for host in host_list]
-
-    # TODO: Parallelize the host connections by creating worker pools and submitting jobs
-    # for host in host_list:
-    #     client(hostname=host, username=args.user, logpath=args.logpath)
+    print('This package can not be called directly')
--- sherman/sherman.py
+++ sherman/sherman.py
@@ -1,12 +1,18 @@
 #!/usr/bin/env python3
 
+import argparse
+from collections import UserDict
+import getpass
+import logging
+import os
+import select
+import socket
+import sys
+import termios
+import tty
+
 import fabric
 import paramiko
-import termios
-import socket
-import select
-import sys
-import tty
 
 from paramiko.py3compat import u
 
@@ -14,16 +20,120 @@ from paramiko.py3compat import u
 # Overview:
 # Sherman connects to remote hosts, and executes the commands within each "job" module through those connections.
 # Optionally, connections may be made through a Proxy server. If a proxy server is utilized, multiple connections
-# mnay be made through a single proxy to keep the total number of connections low.
+# may be made through a single proxy to keep the total number of connections low.
 
 # TODO:
 #   Convert to using Fabric
+#     * c: fabric.Connection
 #   Make a CLI for various common commands
 #     * Create job scripts for common functions
 #     * For script jobs, send script to remote server & run it
 #     * Run common commands on remote server
 #     * Optionally log all output, command output
 #     * Use fabric.group.ThreadingGroup when running commands on multiple servers
+
+DEFAULT_TIMEOUT = 10.0
+DEFAULT_PORT = 22
+PROMPT_SUDO_RE = r'\[sudo\] password for '
+PROMPT_2FA_RE = r'Duo two-factor login'
+
+logging.basicConfig(level=logging.DEBUG)
+logging.getLogger(__name__).setLevel(logging.DEBUG)
+logging.getLogger('paramiko').setLevel(logging.DEBUG)
+
+logger = logging.getLogger()
+logger.setLevel(logging.WARNING)
+
+
+class Entry(object):
+    children: list = []
+    connection: fabric.Connection = None
+    distinct: bool = False
+    hostname: str = ''
+    parent: str = None
+
+    def __init__(self, hostname='', connection=None, distinct=False):
+        self.connection = connection
+        self.distinct = distinct
+        self.hostname = hostname
+
+    def close(self):
+        # Walk child connections, closing each
+        # Close our connection
+        # Remove our entry from the children list of our parent
+        # If parent's children list is empty AND distinct != True, call parent's close() function
+        # Remove our entry from the connection list
+        pass
+
+
+class ConnectionRegistry(UserDict):
+    def connect(self, hostname, distinct=True):
+        # If the hostname already exists in the registry, return its connection value
+        if hostname in self.data:
+            return self.data[hostname].connection
+
+        # Add a ConnectionRegistry entry for this host
+        self.data[hostname]: Entry = Entry(hostname=hostname, connection=None, distinct=distinct)
+
+        ssh_config = fabric.config.SSHConfig().from_path('.sshconfig')
+        fab_config = fabric.Config(ssh_config=ssh_config)
+        fab_config.load_base_conf_files()
+
+        # If a ProxyJump gateway is configured, connect to it, add it to the connection registry,
+        # add our hostname as an entry in the children list, and pass its connection as the
+        # gateway for the current connection.
+        gateway = None
+        if 'proxyjump' in ssh_config.lookup(hostname):
+            gateway_hostname = ssh_config.lookup(hostname)['proxyjump']
+            gateway = self.connect(gateway_hostname, distinct=False)
+            self.data[gateway_hostname].children.append(hostname)
+
+        self.data[hostname].connection = fabric.Connection(hostname, gateway=gateway)
+        return self.data[hostname].connection
+
+    def __disconnect__(self, hostname):
+        """Recursively disconnect hostname and its children
+
+        :param hostname: The hostname to disconnect from
+        :type hostname: str
+        """
+        if hostname in self.data:
+            # Recurse through the children of this connection, and disconnect each of them.
+            for child_name in self.data[hostname].children:
+                self.__disconnect__(hostname)
+
+            # Handle our parent if we have one
+            parent = self.data[hostname].parent
+            if parent:
+                # Remove ourselves from our parent's children list
+                self.data[parent].children.remove(hostname)
+
+            # Disconnect the connection for hostname
+            self.data[hostname].connection.close()
+
+    def disconnect(self, hostname):
+        """Disconnect from hostname, its children, and clean up idle connections"""
+        if hostname in self.data:
+            # Disconnect the connection for hostname and its children
+            self.__disconnect__(hostname)
+
+            # Remove our hostname from our predecessor's children list
+            parent = self.data[hostname].parent
+            if parent:
+                # Recursively close our predecessors if they're no longer in use
+                if not self.data[parent].children and not self.data[parent].distinct:
+                    self.disconnect(parent)
+
+        # Clean up idle connections
+        self.disconnect_idle()
+
+    def disconnect_idle(self):
+        """Close connections that were made as proxies and are no longer needed"""
+        for hostname in self.data:
+            if not self.data[hostname].children and not self.data[hostname].distinct:
+                logger.info(f'Disconnecting idle connection for {hostname}')
+                self.__disconnect__(hostname)
+
 
 def send_file(channel: paramiko.channel.Channel, localpath: str, remotepath: str):
     try:
@@ -91,12 +201,14 @@ def shell(channel: paramiko.channel.Channel):
 
 def client(hostname, username):
     try:
-        c = paramiko.SSHClient()
-        c.load_system_host_keys()
-        c.load_host_keys('sherman_host_keys')
-        c.set_missing_host_key_policy(paramiko.AutoAddPolicy)
-        c.connect(hostname, 22, username, timeout=10, compress=True)
-        c.save_host_keys('sherman_host_keys')
+        c: fabric.Connection = connections.connect(hostname)
+        c.connect_timeout=DEFAULT_TIMEOUT
+
+        # c.load_host_keys('sherman_host_keys')
+        # c.set_missing_host_key_policy(paramiko.AutoAddPolicy)
+        # c.connect(hostname, 22, username, timeout=10, compress=True)
+        # c.save_host_keys('sherman_host_keys')
+
     except paramiko.ssh_exception.AuthenticationException as ex:
         print(f'Error: Authentication error for {username}@{hostname}. {ex}')
     except paramiko.ssh_exception.BadHostKeyException as ex:
@@ -128,6 +240,66 @@ def client(hostname, username):
     # Close the SSH connection
     c.close()
 
+# except paramiko.AuthenticationException as ex:
+#   logger.error(f'Error: Authentication error for {c}. {ex}')
+#   return
+# except paramiko.BadHostKeyException as ex:
+#   print(f'Error: Bad host key for {hostname}. {ex}')
+#   return
+# except paramiko.SSHException as ex:
+#   print(f'Error: SSH exception for {hostname}. {ex}')
+#   return
+# except socket.error as ex:
+#   print(f'Error: Socket error for {hostname}. {ex}')
+#   return
+# finally:
+#   # Close the SSH connection
+#   close_connection(hostname)
+
+
+connections = ConnectionRegistry()
+
 
 if __name__ == '__main__':
+    host_list = []
+    # TODO:
+    #   Use Fabric Group management to handle a pool of devices
+    #     * fabric.group.ThreadingGroup
+    #   Retrieve results from commands and write to local per-server log files
+
+    # TODO: When user is NONE, use the SSH config, then fall back to current OS user.
+    # TODO: If user _is_ specified, use it before SSH config.
+    ap = argparse.ArgumentParser(description='Collect SMART data from remote hosts.')
+    ap.add_argument('-d', '--debug', action='store_true', help='Enable diagnostic output.')
+    ap.add_argument('-H', '--hostfile', nargs='?', type=str, help='File containing a list of hosts.')
+    ap.add_argument('hosts', nargs='*', type=str, help='One or more hosts to poll.')
+    ap.add_argument('-l', '--logpath', nargs='?', type=str, help='Path to output host log files.')
+    ap.add_argument('-p', '--parallel', type=int, default=10, help='Path to output host log files.')
+    ap.add_argument('-u', '--user', type=str, default=getpass.getuser(), help='Remote host username.')
+    args = ap.parse_args()
+
+    if args.logpath is not None and args.logpath != '':
+        if not os.path.isdir(args.logpath):
+            print(f'Error: Log path {args.logpath} does not exist.')
+            sys.exit(1)
+
+    if args.hostfile is not None:
+        # A file of hosts was specified on the command line
+        with open(args.hostfile, 'r') as host_file:
+            host_lines = host_file.readlines()
+            for host_line in host_lines:
+                host_list.append(host_line.strip())
+    elif args.hosts:
+        # A list of hosts was specified on the command line
+        host_list = args.hosts.copy()
+    else:
+        # The command line list is empty, and there was no host file specified
+        print('Error: You must specify a host, or a file containing a list of hosts.')
+        ap.print_help()
+        sys.exit(1)
+
+    if args.parallel < 1:
+        print(f'Error: Parallel must be greater than 0.')
+        ap.print_help()
+        sys.exit(1)
     client("server.domain.tld", "tom")
